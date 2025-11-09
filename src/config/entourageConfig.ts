ad08ccd62d47019ef91fb2013ced20956a3561bf
Gene De La Rea <jear200117@gmail.com>
2025-11-09 15:07:27 +0800

    Fix name in entourageConfig.ts from 'Daisy Pagcaliwangan' to 'Eudosia Pagcaliwangan'

--- src/config/entourageConfig.ts
+++ src/config/entourageConfig.ts
@@ -55,7 +55,7 @@ export const ENTOURAGE_CONFIG = {
         { name: "Remedios Punongbayan", number: 1 },
         { name: "Marilyn Palas", number: 2 },
         { name: "Janelle Saldaña", number: 3 },
-        { name: "Daisy Pagcaliwangan", number: 4 },
+        { name: "Eudosia Pagcaliwangan", number: 4 },
         { name: "Christina De La Rea", number: 5 },
         { name: "Maricel Quinag", number: 6 },
         { name: "Imelda Monteza", number: 7 },
